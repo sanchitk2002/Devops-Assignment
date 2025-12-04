9e7d360db6e2e9eb2d9b45b6bb42199c3af47e85
Sanchit <sanchitk330@gmail.com>
2025-12-04 16:20:11 +0530

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,5 @@
+file = open("hello.txt", "w")
+file.write("Hey guys, this is a sample text file.")
+file.close()
+
+print("File created and content written.")
---
+++ Task4.py
@@ -0,0 +1,6 @@
+file = open("hello.txt", "r")
+content = file.read()
+file.close()
+
+print("File Content:")
+print(content)
